6a64f1456c70bf58db4e8c2fbfb98d5f12cb5df0
Florian <floriansieradzki@gmail.com>
2025-02-21 11:40:13 +0100

    add a spinner

---
+++ js/components/App.js
@@ -0,0 +1,8 @@
+﻿import AppButton from "./AppButton.js";
+
+export default {
+    components: {
+        'app-button': AppButton,
+    }
+
+}
---
+++ js/components/AppButton.js
@@ -0,0 +1,25 @@
+﻿export default {
+    template: `
+        <button 
+        :class="{
+        ' rounded px-5 py-2 disabled:cursor-not-allowed':  true,
+        'bg-blue-200 hover:bg-blue-400 border': type === 'primary',
+        'bg-purple-200 hover:bg-purple-400 border': type === 'secondary',
+        'bg-gray-200 hover:bg-gray-400 border': type === 'muted',
+        'is-loading': processing
+        }" 
+        :disabled="processing">
+            <slot />
+        </button>
+    `,
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+        processing:{
+            type: Boolean,
+            default: false
+        }
+    },
+}
